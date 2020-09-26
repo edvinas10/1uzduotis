d5394452d633689ef1a13432e55ba7ec891fa8c3
edvinas10 <71633786+edvinas10@users.noreply.github.com>
2020-09-26 14:56:23 +0300

    Add files via upload

--- v01.cpp
+++ v01.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
-int egzas;
-int n;
-string vardas, pavarde;
-int vidurkis = 0;
-int laik;
-cin >> vardas >> pavarde;
-cin >> n;
-for(int x=1; x<=n; x++){
-  cin >> laik;
-  vidurkis = vidurkis + laik;
-} cin egzas;
-galutinis = 0.4*vidurkis\n+0.6*egzas;
-cout << galutinis << end;
+using std::cout;
+using std::cin;
+using std::string;
+struct duomuo {
+  string Vard;
+  string Pav;
+  int paz[10];
+  int egz;
+  float GP=0;
+};
+
+int main()
+{
+  duomuo Eil; duomuo Eil_mas[5];
+  std::vector <duomuo> Eil_vect;
+  cout << "Iveskite Eil. duom. (Vardas, Pavarde, egz. paz. ir 5 sem. paz.):\n";
+  cin >> Eil.Vard >> Eil.Pav >> Eil.egz;
+  for (int i = 0; i < 5; i++) { 
+    cin >> Eil.paz[i];
+    Eil.GP = Eil.GP + (float)Eil.paz[i];
+  };
+  Eil.GP = Eil.GP / 5.0;
+  Eil.GP = Eil.GP * 0.4 + 0.6 * Eil.egz;
+  cout << "Ivesta:" << Eil.Vard << " " << Eil.Pav << " " << Eil.egz;
+  for (int i = 0; i < 5; i++){  
+    cout << " " << Eil.paz[i];
+  };
+    cout << " " << Eil.GP << std::endl;
+}
